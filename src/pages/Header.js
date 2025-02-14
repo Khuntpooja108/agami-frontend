d658c1d4fe671247cf78db95cb347f51ec2e7c2d
pooja <khuntpooja108@gmail.com>
2025-02-14 18:05:06 +0530

    commit

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -2,14 +2,14 @@ import { Outlet } from "react-router-dom";
 
 function Header({ isLoggedIn, setIsLoggedIn }) {
   const profile=JSON.parse(localStorage.getItem("profile"))
-  console.log(profile.name);
+  console.log(profile?.name);
   
   return (
     <>
       <header className="sticky top-0 left-0 z-10 bg-white shadow px-6 py-4 flex justify-between items-center transition-all">
         <div className="flex items-center gap-4 ml-auto">
           <button className="text-gray-600">🔔</button>
-          <span className="text-gray-700 font-medium">{profile.name}</span>
+          <span className="text-gray-700 font-medium">{profile?.name}</span>
 
           <button
             onClick={() => setIsLoggedIn(!isLoggedIn)}
